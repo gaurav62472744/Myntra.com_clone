25d5ec910ab084716026adfd06a58f8197d376df
jitendra4545 <jitendra45.js45@gmail.com>
2023-01-18 23:05:50 +0530

    added day-2 final

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
 
       {/* <MainRoutes /> */}
-      <LargeWithAppLinksAndSocial />
+      {/* <LargeWithAppLinksAndSocial /> */}
 
       <Navbar />
       <MainRoutes />
