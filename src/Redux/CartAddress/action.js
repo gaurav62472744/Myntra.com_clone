0e36787d0940f862f7fba403056f0d6697394c6a
Singh Gaurav Ashokkumar <110033104+gaurav62472744@users.noreply.github.com>
2023-01-18 23:15:05 +0530

    api updated

--- src/Redux/CartAddress/action.js
+++ src/Redux/CartAddress/action.js
@@ -1,47 +1,46 @@
-import { ADD_ADDRESS_SUCCESS, GET_ADDRESS_SUCCESS } from "./actionTypes"
-import axios from 'axios'
+import { ADD_ADDRESS_SUCCESS, GET_ADDRESS_SUCCESS } from "./actionTypes";
+import axios from "axios";
 
+const addAddressSuccess = (payload) => {
+  return {
+    type: ADD_ADDRESS_SUCCESS,
+    payload,
+  };
+};
 
+const getaddressSuccess = (payload) => {
+  return {
+    type: GET_ADDRESS_SUCCESS,
+    payload,
+  };
+};
 
-const addAddressSuccess=(payload)=>{
-      return {
-        type:ADD_ADDRESS_SUCCESS,payload
-      }
-}
+export const getAddres = () => (dispatch) => {
+  axios.get(`https://wadrobe.onrender.com/address`).then((res) => {
+    console.log(res.data);
+    dispatch(getaddressSuccess(res.data));
+  });
+};
 
-const getaddressSuccess=(payload)=>{
-   return {
-      type:GET_ADDRESS_SUCCESS,payload
-   }
-}
-
-
-export const getAddres=()=>(dispatch)=>{
-axios.get(`http://localhost:8080/address`)
-.then((res)=>{
-   console.log(res.data)
-  dispatch(getaddressSuccess(res.data))
-})
-}
-
-
-
-export const addAddress=(name,mobile,pinCode,address,locality,city,state)=>(dispatch)=>{
-     const payload={
-             name,
-             mobile,
-             pinCode,
-             address,
-             locality,
-             city,
-             state,
-             default:'Home'
-     }
-     axios.post(`http://localhost:8080/address`,payload)
-     .then((res)=>{
-        console.log(res)
-        dispatch(addAddressSuccess(res.data))
-     }).catch((err)=>{
-        console.log(err)
-     })
-}
+export const addAddress =
+  (name, mobile, pinCode, address, locality, city, state) => (dispatch) => {
+    const payload = {
+      name,
+      mobile,
+      pinCode,
+      address,
+      locality,
+      city,
+      state,
+      default: "Home",
+    };
+    axios
+      .post(`https://wadrobe.onrender.com/address`, payload)
+      .then((res) => {
+        console.log(res);
+        dispatch(addAddressSuccess(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
