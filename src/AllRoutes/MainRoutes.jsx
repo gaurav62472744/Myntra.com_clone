325e228c4b2252616d68c2fd4c80c7d0de10d76d
Singh Gaurav Ashokkumar <110033104+gaurav62472744@users.noreply.github.com>
2023-01-19 19:58:50 +0530

    added module

--- src/AllRoutes/MainRoutes.jsx
+++ src/AllRoutes/MainRoutes.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import AdminStore from "../Pages/AdminStore";
 import { CartAddress } from "../Pages/CartAddress";
 import { CartPayment } from "../Pages/CartPayment";
+import Dashboard from "../Pages/Dashboard";
 import Homepage from "../Pages/Homepage";
 import NotFound from "../Pages/NotFound";
+import TrendingPro from "../Pages/TrendingPro";
 
 const MainRoutes = () => {
   return (
@@ -11,8 +14,11 @@ const MainRoutes = () => {
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="*" element={<NotFound />}></Route>
-        <Route path='/cartaddress' element={<CartAddress/>} />
-        <Route path="/cartpayment" element={<CartPayment/>} />
+        <Route path="/cartaddress" element={<CartAddress />} />
+        <Route path="/cartpayment" element={<CartPayment />} />
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/trending" element={<TrendingPro />}></Route>
+        <Route path="/storeproduct" element={<AdminStore />}></Route>
       </Routes>
     </div>
   );
