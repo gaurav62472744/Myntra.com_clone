5bddd8024560ee2b50b1dfd4e701116fb6668092
Singh Gaurav Ashokkumar <110033104+gaurav62472744@users.noreply.github.com>
2023-01-20 03:59:28 +0530

    admin redux modified

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,12 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { reducer as CartAddress } from "./CartAddress/reducer";
 import { reducer as ProductReducer } from "./Product/reducer";
-import thunk from 'redux-thunk'
-const rootReducer=combineReducers({CartAddress,ProductReducer})
+import { reducer as AdminReducer } from "./Admin/reducer";
+import thunk from "redux-thunk";
+const rootReducer = combineReducers({
+  CartAddress,
+  ProductReducer,
+  AdminReducer,
+});
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
